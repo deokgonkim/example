d5e323e37615c8329b158c952101dfb2e3d6c5b8
Deokgon Kim <dgkim@dgkim.net>
2022-07-29 14:59:26 +0900

    add swiftui-hello

---
+++ helloworld/swift/swiftui-hello/swiftui-hello/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftui-hello
+//
+//  Created by Deokgon Kim on 2022/07/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ helloworld/swift/swiftui-hello/swiftui-hello/swiftui_helloApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_helloApp.swift
+//  swiftui-hello
+//
+//  Created by Deokgon Kim on 2022/07/29.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_helloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
