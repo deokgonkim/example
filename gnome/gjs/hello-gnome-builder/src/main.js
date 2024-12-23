9e744d4939b5ec35e821fd5e5131f3a89a2702d5
Deokgon Kim <dgkim@dgkim.net>
2024-12-23 16:20:37 +0900

    add gjs/hello-gnome-builder

---
+++ gnome/gjs/hello-gnome-builder/src/main.js
@@ -0,0 +1,76 @@
+/* main.js
+ *
+ * Copyright 2024 dgkim
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+import GObject from 'gi://GObject';
+import Gio from 'gi://Gio';
+import Gtk from 'gi://Gtk?version=4.0';
+import Adw from 'gi://Adw?version=1';
+
+import { HelloWindow } from './window.js';
+
+pkg.initGettext();
+pkg.initFormat();
+
+export const HelloApplication = GObject.registerClass(
+    class HelloApplication extends Adw.Application {
+        constructor() {
+            super({application_id: 'net.dgkim.gnome.Hello', flags: Gio.ApplicationFlags.DEFAULT_FLAGS});
+
+            const quit_action = new Gio.SimpleAction({name: 'quit'});
+                quit_action.connect('activate', action => {
+                this.quit();
+            });
+            this.add_action(quit_action);
+            this.set_accels_for_action('app.quit', ['<primary>q']);
+
+            const show_about_action = new Gio.SimpleAction({name: 'about'});
+            show_about_action.connect('activate', action => {
+                let aboutParams = {
+                    transient_for: this.active_window,
+                    application_name: 'hello',
+                    application_icon: 'net.dgkim.gnome.Hello',
+                    developer_name: 'dgkim',
+                    version: '0.1.0',
+                    developers: [
+                        'dgkim'
+                    ],
+                    copyright: '© 2024 dgkim'
+                };
+                const aboutWindow = new Adw.AboutWindow(aboutParams);
+                aboutWindow.present();
+            });
+            this.add_action(show_about_action);
+        }
+
+        vfunc_activate() {
+            let {active_window} = this;
+
+            if (!active_window)
+                active_window = new HelloWindow(this);
+
+            active_window.present();
+        }
+    }
+);
+
+export function main(argv) {
+    const application = new HelloApplication();
+    return application.runAsync(argv);
+}
