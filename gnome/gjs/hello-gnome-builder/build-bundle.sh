565a219dfe4d21bf846ea5b9c2ba5e4669845c65
Deokgon Kim <dgkim@dgkim.net>
2024-12-23 16:41:48 +0900

    update gnome/gjs/hello-gnome-builder

---
+++ gnome/gjs/hello-gnome-builder/build-bundle.sh
@@ -0,0 +1,2 @@
+flatpak build-bundle repo hello.flatpak net.dgkim.gnome.Hello --runtime-repo=https://flathub.org/repo/flathub.flatpakrepo
+
---
+++ gnome/gjs/hello-gnome-builder/build.sh
@@ -0,0 +1,2 @@
+flatpak-builder --force-clean --user --install-deps-from=flathub --install builddir --repo=repo net.dgkim.gnome.Hello.json
+
