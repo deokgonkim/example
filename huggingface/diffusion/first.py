cb9773a9efa15dfefd58c72cc97a1cd0779beeb8
Deokgon Kim <dgkim@dgkim.net>
2025-05-31 12:26:50 +0900

    update huggingface/diffusion/first.py

--- huggingface/diffusion/first.py
+++ huggingface/diffusion/first.py
@@ -1,3 +1,4 @@
+import datetime
 import os
 from dotenv import load_dotenv
 load_dotenv(os.path.join(os.path.dirname(__file__), ".env"))
@@ -10,13 +11,14 @@ secret = os.getenv("HUGGINGFACE_ACCESS_TOKEN")
 
 pipe = StableDiffusion3Pipeline.from_pretrained("stabilityai/stable-diffusion-3-medium-diffusers", torch_dtype=torch.float16, token=secret)
 # pipe = pipe.to("cuda")
+pipe = pipe.to("mps") # Apple M1 Metal
 
 image = pipe(
     "A cat holding a sign that says hello world",
     negative_prompt="",
     num_inference_steps=28,
     guidance_scale=7.0,
-    width=16,
-    height=16
+    width=256,
+    height=256
 ).images[0]
-image.save("output.png")
+image.save(os.path.join('output', f'first-{datetime.datetime.now().isoformat()}.png'))
