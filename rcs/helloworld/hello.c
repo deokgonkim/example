b2576cc53ae25950afd8f4b559f06bce4946a682
Deokgon Kim <dgkim@dgkim.net>
2025-12-12 10:16:30 +0900

    add rcs example

---
+++ rcs/helloworld/hello.c
@@ -0,0 +1,18 @@
+/**
+ * $Id: hello.c,v 1.2 2024/06/10 08:12:49 eric Exp eric $
+ *
+ * $Log: hello.c,v $
+ * Revision 1.2  2024/06/10 08:12:49  eric
+ * print hello again
+ *
+ * Revision 1.1  2024/06/10 08:06:08  eric
+ * Initial revision
+ *
+ */
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+	printf("Hello world\n");
+	printf("Hello world\n");
+}
+
